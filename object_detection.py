ee98f954eb6d36403395a9c38cb20f3acda0fe04
Yauheni Kavaliou <bu4er88@gmail.com>
2021-07-23 14:22:41 +0300

    files added

---
+++ object_detection.py
@@ -0,0 +1,34 @@
+import cvlib
+from cvlib.object_detection import draw_bbox
+import cv2
+import time
+
+
+cap = cv2.VideoCapture(0)  # capture from webcam
+
+start = current = 0
+
+while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+        print('Capturing is stopped')
+        break
+
+    bbox, labels, confidence = cvlib.detect_common_objects(
+        image, confidence=0.3, nms_thresh=0.5, model='yolov4-tiny', enable_gpu=False)
+
+
+    output = draw_bbox(image, bbox, labels, confidence, colors=None, write_conf=False)
+
+    current = time.time()
+    fps = 1 / (current - start)
+    start = current
+    cv2.putText(image, 'FPS' + str(int(fps)), (5, 25), cv2.FONT_ITALIC, 0.7, (255,255,255), 2)
+
+    cv2.imshow('output', output)
+
+    if cv2.waitKey(5) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
